520e708ba43f89334e9764d18fe79400084b0c8a
WenZhi <weznzhi.24@ichat.sp.edu.sg>
2025-05-12 22:54:56 +0800

    return values implemented

--- bmi.py
+++ bmi.py
@@ -8,9 +8,13 @@ def calculate_bmi(height, weight):
 
   if bmi <18.5:
     print("Underweight")
+    return -1
   elif bmi < 25:
     print("Normal weight")
+    return 0
   elif bmi >= 25:
     print("Overweight")
+    return 1
+# Test the function
 
 calculate_bmi(1.73, 57)
